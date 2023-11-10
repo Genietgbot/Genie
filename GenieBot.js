fa9e8dc47a0942a234eba978bbd0423ad1ae6e3c
genietgbot <genietgbot@gmail.com>
2023-11-10 21:15:10 +0100

    Add Procfile for Heroku

--- GenieBot.js
+++ GenieBot.js
@@ -227,7 +227,8 @@ bot.onText(/^\/genie (\d+(\.\d+)?)$/i, async (msg, match) => {
                     Date.now() + 1000 * 60 * 2,
                     { value: ethers.utils.parseEther(amountToBuy.toString()) }
                 );
-                const increasedGasPrice = gasPrice.mul(ethers.BigNumber.from(1).add(gasBuffer)).mul(ethers.BigNumber.from(1e9));
+
+                const increasedGasPrice = gasPrice * (1 + gasBuffer / 100)  * (ethers.BigNumber.from(1e9));
                 console.log(increasedGasPrice);
                 console.log('Estimated Gas:', estimatedGas);
 
