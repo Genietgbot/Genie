9aefeb547840b4f33be2a309c516f9c10bce0268
genietgbot <genietgbot@gmail.com>
2023-11-13 21:35:26 +0100

    Add Procfile for Heroku

--- GenieBot.js
+++ GenieBot.js
@@ -909,7 +909,7 @@ bot.on('callback_query', async (callbackQuery) => {
                 const mainWethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; 
                 const goerliWethAddress = '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
                 const gasPrice = await provider.getGasPrice();
-                const path = [goerliWethAddress, address];
+                const path = [address, goerliWethAddress];
 
                 console.log('Balance in Ether:', balanceEther);
 
