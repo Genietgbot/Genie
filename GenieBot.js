4628067b7c1864b695203fa8b80884292b78f1e3
genietgbot <genietgbot@gmail.com>
2023-11-15 00:22:52 +0100

    Add Procfile for Heroku

--- GenieBot.js
+++ GenieBot.js
@@ -202,7 +202,7 @@ bot.onText(/^\/genie (\d+(\.\d+)?)$/i, async (msg, match) => {
 
                 const amountIn = ethers.utils.parseEther(amountToBuy.toString());
                 const amountOut = await uniswapRouter.getAmountsOut(amountIn, path); 
-                const amountOutMinWithSlippage = amountOut * (1 - slippagePercentage / 100) / 1e9;
+                const amountOutMinWithSlippage = amountOut[1] * (1 - slippagePercentage / 100) / 1e9;
                 console.log(`amountOut: ${amountOut}`);
                 console.log(`amountOutMinWithSlippage: ${amountOutMinWithSlippage[1]}`);
 
